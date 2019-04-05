90627826ccaf4053af1233b16fa2f7d21dbb48d5
johnny3young <youngerblack@gmail.com>
2019-04-05 14:55:29 -0500

    Primer proyecto de Kotlin enviado desde donde cuarto

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+
+
+fun  main (args: Array<String>){
+
+    //Variables
+    var n:Int = 3
+    var d:Double = 2.5
+    var f:Float = 1.5f
+    var verdad = true
+    var x:Char = 'x'
+    var y:String = "x"
+
+
+    //Constantes
+    val v = 10
+
+    //Arreglos
+    var arreglo01 = arrayOf(1,2,3,4,5,6,7,8,9)
+    
+
+    println("Hola Mundo!!!"+"\n" + n + "\n"+ d + "\n"+ f + "\n"+ x + "\n"+ y +"\n" + arreglo01[8]);
+
+}
+
